56627475d136b8dac6ec41c4b11ff87bd4088557
Johnny Chevalier <johnny.chevalier91@gmail.com>
2024-07-18 09:32:40 +0200

    Modif barre de navigation

--- script/script.js
+++ script/script.js
@@ -2,3 +2,18 @@ function toggleMenu() {
   const navbar = document.querySelector(".navbar");
   navbar.classList.toggle("active");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuItems = document.getElementById("bropbtn");
+
+  menuItems.forEach(function (menuItem) {
+    menuItem.addEventListener("click", function () {
+      const dropbtn = this.getElementById("dropbtn");
+      if (dropbtn.style.display === "block") {
+        dropbtn.style.display = "none";
+      } else {
+        dropbtn.style.display = "block";
+      }
+    });
+  });
+});
